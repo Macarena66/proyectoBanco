a3a2f0f715edf532f9d71f53697c8c52b912efda
Macarena <macarenachaves@gmail.com>
2022-06-10 19:49:05 +0200

    Primer commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const gestor1 = {
+    id:1,
+    usuario:'gestor1',
+    password:'gestor1',
+    correo:'gestor1@gmail.com'
+};
+const gestor2 ={
+    id:2,
+    usuario:'gestor2',
+    password:'gestor2',
+    correo:'gestor2@gmail.com'
+};
+
+const cliente1={
+    id:01,
+    id_gestor:2,
+    usuario: 'Macarena',
+    password:'macarena01',
+    correo:'macarena@gmail.com',
+    saldo:8000
+};
+
+const cliente2={
+    id:02,
+    id_gestor:1,
+    usuario:'Victor',
+    password:'Victor02',
+    correo:'victor@gmail.com',
+    saldo:5000
+}
+
+const cliente3={
+    id:03,
+    id_gestor:1,
+    usuario:'Marina',
+    password:'Marina03',
+    correo:'marina@gmail.com',
+    saldo:4000
+}
+console.log(gestor1);
+console.log(cliente2);
