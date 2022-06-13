4501f917384824e68ead6cb07ec72368109090c0
Macarena <macarenachaves@gmail.com>
2022-06-13 19:12:32 +0200

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*
+Escribe un programa que almacene los objetos creados en el ejercio anterior del proyecto dentro de un array (un array por cada modelo de datos). A continuación, recorre cada uno de los arrays y muestra todas propiedades..
+*/
+
 const gestor1 = {
     id:1,
     usuario:'gestor1',
@@ -37,5 +41,49 @@ const cliente3={
     correo:'marina@gmail.com',
     saldo:4000
 }
-console.log(gestor1);
-console.log(cliente2);
+
+const clientes = [gestor1, gestor2, {
+    id: 3,
+    usuario: 'gestor3',
+    password: 'gestor3',
+    correo: 'gestor3@mail.com'
+}];
+
+const clientes = [cliente1, cliente2, cliente3];
+clientes.push({
+    id: 04,
+    id_gestor: 1,
+    usuario: 'cliente4',
+    password: 'cliente4',
+    correo:'cliente4@gmail.com',
+    saldo: 1230  
+});
+// gestores es un array
+// gestor es un objeto con las propiedades: id, usuario, password, correo
+console.log('---GESTORES---------');
+for (const gestor of clientes){
+    console.log(`id: ${gestor.id}`);
+    console.log(`usuario: ${gestor.usuario}`);
+    console.log(`password: ${gestor.password}`);
+    console.log(`correo: ${gestor.correo}`);
+    console.log('-----');
+}
+console.log('---CLIENTES---------');
+for (const cliente of clientes){
+    console.log(`id: ${cliente.id}`);
+    console.log(`usuario: ${cliente.usuario}`);
+    console.log(`password: ${cliente.password}`);
+    console.log(`correo: ${cliente.correo}`);
+    console.log(`saldo: ${cliente.saldo}`);
+    console.log('-----');
+    
+}
+
+
+// todos los gestores
+//console.log(gestores);
+
+// primer gestor
+//console.log(gestores[0]);
+//console.log(gestor1);
+//console.log(cliente2);
