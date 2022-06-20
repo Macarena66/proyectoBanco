448d7c9376421a459e4168c72e03d31e9959e16d
Macarena <macarenachaves@gmail.com>
2022-06-20 20:27:53 +0200

    Ejercicio 4.2

--- main.js
+++ main.js
@@ -30,56 +30,21 @@ Escribe un programa que almacene los objetos creados en el ejercio anterior del 
 // })
 
 
-const server = 'localhost:8085';
 
-const opciones = {
-    url: `http://${server}/ok`,
-    metodo: 'GET'
-};
 
-ajax(opciones, (data) => {
-    console.log(data);
-});
+// obtenerGestores(gestores => {
+//     mostrarGestores(gestores);
+// });
 
-const opcionesLogin = {
-    url: `http://${server}/login/gestor/`,
-    metodo: 'POST',
-    body: 'usuario=gestor1&password=gestor1',
-    cabeceras: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-};
+async function main() {
+    const gestores = await obtenerGestores();
+    mostrarGestores(gestores);
+}
 
-// realiza la petición de login del gestor
-ajax(opcionesLogin, (data) => {
+main();
 
-    // esta función se ejecuta cuando los datos han sido
-    // devueltos por el servicio
-
-    // convirtiendo el string en formato JSON a objeto de JavaScript
-    const respuesta = JSON.parse(data);
-
-    // guardamos el token
-    const token = respuesta.data.token;
-
-// realizamos la solicitud para obtener todos los gestores
-    const opcionesObtenerGestores = {
-        url: 'http://localhost:8085/gestores/',
-        metodo: 'GET',
-        cabeceras: {
-            Authorization: `Basic ${token}`
-        }
-    }
-
-    ajax(opcionesObtenerGestores, (data) => {
-
-         // data tiene toda la información de los gestores en formato string
-        console.log(data);
-
-         // convertir a objeto de JavaScript
-         const respuesta = JSON.parse(data);
-
-         const gestores = respuesta.data;
-         mostrarGestores(gestores);
-    });
-});
+// función ejecutable
+// (async () => {
+//     const gestores = await obtenerGestores();
+//     mostrarGestores(gestores);
+// })();
